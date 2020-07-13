f4dda49b0271180464ac93e998edf7334a2568e6
MG <godsonmuluh@gmail.com>
2020-07-13 17:57:10 +0100

    middleware bug fix
    middleware bug fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'Admin' => \App\Http\Middleware\Admin::class,
-        'superAdmin' => \App\Http\Middleware\SuperAdmin::class,
+        'superAdmin' => \App\Http\Middleware\superAdmin::class,
         
     ];
 }
