182f0df03f85ab3f8ec31541551a4d9bccdd2c86
MG <godsonmuluh@gmail.com>
2020-07-23 00:31:32 +0100

    sidebar temporal fix for admin mobile view
    sidebar temporal fix for admin mobile view

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
   <div class="row">
     @can('isVendor')
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 position-fixed d-md-block bg-light sidebar collapse">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div class="sidebar-sticky pt-1">
         <ul class="nav flex-column">
           <li class="nav-item">
--- resources/views/admin/layouts/topbar.blade.php
+++ resources/views/admin/layouts/topbar.blade.php
@@ -1,17 +1,16 @@
 <header class="sticky-top">
 	<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
   		<a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="{{ route('admin.dashboard') }}"><i class="fas fa-shopping-cart"></i> SchoolFAQs Shop | Control Panel</a>
-  		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+  		<button class="navbar-toggler d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-  		<input class="form-control form-control-dark w-50" type="text" placeholder="Search" aria-label="Search">
   		<ul class="navbar-nav px-3">
-  			<li class="nav-item text-nowrap text-light">
+  			<li class="nav-item text-nowrap d-xs-none text-light">
     			<i class="fas fa-user"></i> Welcome {{Auth::user()->name}}
     		</li>
     		<li class="nav-item text-nowrap">
           <form  action="{{ route('logout') }}" method="POST">
-            <input type="submit" class="btn form-control btn-danger mb-2" value="Sign Out">
+            <input type="submit" class="btn form-control float-right btn-danger mb-2" value="Sign Out">
               @csrf
           </form>
       			
